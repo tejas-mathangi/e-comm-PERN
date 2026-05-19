afabc92938a9a9c041dab6479723bb1562be3738
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-19 16:18:04 +0530

    feat : added auth and other middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+
+app.use(clerkMiddleware()); // auth obj
+app.use(express.json()); // parses JSON req bodies
+app.use(express.urlencoded({ extended: true })); // parses form data (like html form)
 
 app.get("/", (req, res) => {
   res.json({
