2a20dbfa7bc4d07ab514ba26a58176ff5516d430
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-19 12:51:20 +0530

    feat : add basic express server with root endpoint

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to productify API - PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/user",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
