6c1bc6ef20e519450b45b430be7957344d9db4b3
Tejas Mathangi <mathangi.tejas@gmail.com>
2026-05-19 16:16:42 +0530

    chore : added db and auth secret keys

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,4 +6,6 @@ export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
   NODE_ENV: process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 };
